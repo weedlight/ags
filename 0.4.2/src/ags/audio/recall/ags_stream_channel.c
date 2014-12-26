587c7435e605dc08f8c7e835a6910fc796b863ae
Joël Krähemann <joelkrahemann@Joels-MacBook-Pro.local>
2014-12-26 15:32:31 +0100

    added 0.4.2

---
+++ 0.4.2/src/ags/audio/recall/ags_stream_channel.c
@@ -0,0 +1,178 @@
+/* AGS - Advanced GTK Sequencer
+ * Copyright (C) 2005-2011 Joël Krähemann
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <ags/audio/recall/ags_stream_channel.h>
+
+#include <ags-lib/object/ags_connectable.h>
+
+#include <ags/main.h>
+
+#include <ags/object/ags_plugin.h>
+
+void ags_stream_channel_class_init(AgsStreamChannelClass *stream_channel);
+void ags_stream_channel_connectable_interface_init(AgsConnectableInterface *connectable);
+void ags_stream_channel_plugin_interface_init(AgsPluginInterface *plugin);
+void ags_stream_channel_init(AgsStreamChannel *stream_channel);
+void ags_stream_channel_connect(AgsConnectable *connectable);
+void ags_stream_channel_disconnect(AgsConnectable *connectable);
+void ags_stream_channel_finalize(GObject *gobject);
+
+/**
+ * SECTION:ags_stream_channel
+ * @short_description: streams channel
+ * @title: AgsStreamChannel
+ * @section_id:
+ * @include: ags/audio/recall/ags_stream_channel.h
+ *
+ * The #AgsStreamChannel class provides ports to the effect processor.
+ */
+
+static gpointer ags_stream_channel_parent_class = NULL;
+static AgsConnectableInterface *ags_stream_channel_parent_connectable_interface;
+static AgsPluginInterface *ags_stream_channel_parent_plugin_interface;
+
+GType
+ags_stream_channel_get_type()
+{
+  static GType ags_type_stream_channel = 0;
+
+  if(!ags_type_stream_channel){
+    static const GTypeInfo ags_stream_channel_info = {
+      sizeof (AgsStreamChannelClass),
+      NULL, /* base_init */
+      NULL, /* base_finalize */
+      (GClassInitFunc) ags_stream_channel_class_init,
+      NULL, /* class_finalize */
+      NULL, /* class_data */
+      sizeof (AgsStreamChannel),
+      0,    /* n_preallocs */
+      (GInstanceInitFunc) ags_stream_channel_init,
+    };
+
+    static const GInterfaceInfo ags_connectable_interface_info = {
+      (GInterfaceInitFunc) ags_stream_channel_connectable_interface_init,
+      NULL, /* interface_finalize */
+      NULL, /* interface_data */
+    };
+
+    static const GInterfaceInfo ags_plugin_interface_info = {
+      (GInterfaceInitFunc) ags_stream_channel_plugin_interface_init,
+      NULL, /* interface_finalize */
+      NULL, /* interface_data */
+    };    
+
+    ags_type_stream_channel = g_type_register_static(AGS_TYPE_RECALL_CHANNEL,
+						     "AgsStreamChannel\0",
+						     &ags_stream_channel_info,
+						     0);
+
+    g_type_add_interface_static(ags_type_stream_channel,
+				AGS_TYPE_CONNECTABLE,
+				&ags_connectable_interface_info);
+
+    g_type_add_interface_static(ags_type_stream_channel,
+				AGS_TYPE_PLUGIN,
+				&ags_plugin_interface_info);
+  }
+
+  return (ags_type_stream_channel);
+}
+
+void
+ags_stream_channel_class_init(AgsStreamChannelClass *stream_channel)
+{
+  GObjectClass *gobject;
+  AgsRecallClass *recall;
+
+  ags_stream_channel_parent_class = g_type_class_peek_parent(stream_channel);
+
+  /* GObjectClass */
+  gobject = (GObjectClass *) stream_channel;
+
+  gobject->finalize = ags_stream_channel_finalize;
+}
+
+void
+ags_stream_channel_connectable_interface_init(AgsConnectableInterface *connectable)
+{
+  ags_stream_channel_parent_connectable_interface = g_type_interface_peek_parent(connectable);
+
+  connectable->connect = ags_stream_channel_connect;
+  connectable->disconnect = ags_stream_channel_disconnect;
+}
+
+void
+ags_stream_channel_plugin_interface_init(AgsPluginInterface *plugin)
+{
+  ags_stream_channel_parent_plugin_interface = g_type_interface_peek_parent(plugin);
+}
+
+void
+ags_stream_channel_init(AgsStreamChannel *stream_channel)
+{
+  AGS_RECALL(stream_channel)->name = "ags-stream\0";
+  AGS_RECALL(stream_channel)->version = AGS_EFFECTS_DEFAULT_VERSION;
+  AGS_RECALL(stream_channel)->build_id = AGS_BUILD_ID;
+  AGS_RECALL(stream_channel)->xml_type = "ags-stream-channel\0";
+  AGS_RECALL(stream_channel)->port = NULL;
+}
+
+void
+ags_stream_channel_finalize(GObject *gobject)
+{
+  /* empty */
+
+  /* call parent */
+  G_OBJECT_CLASS(ags_stream_channel_parent_class)->finalize(gobject);
+}
+
+void
+ags_stream_channel_connect(AgsConnectable *connectable)
+{
+  ags_stream_channel_parent_connectable_interface->connect(connectable);
+
+  /* empty */
+}
+
+void
+ags_stream_channel_disconnect(AgsConnectable *connectable)
+{
+  ags_stream_channel_parent_connectable_interface->disconnect(connectable);
+
+  /* empty */
+}
+
+/**
+ * ags_stream_channel_new:
+ *
+ * Creates an #AgsStreamChannel
+ *
+ * Returns: a new #AgsStreamChannel
+ *
+ * Since: 0.4
+ */
+AgsStreamChannel*
+ags_stream_channel_new()
+{
+  AgsStreamChannel *stream_channel;
+
+  stream_channel = (AgsStreamChannel *) g_object_new(AGS_TYPE_STREAM_CHANNEL,
+						     NULL);
+
+  return(stream_channel);
+}
